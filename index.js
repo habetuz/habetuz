101fe994e89925903be7e0e331b60e49ab2dddfb
Marvin Fuchs <marfinn3@gmail.com>
2021-02-04 19:40:24 +0100

    Expand logo

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 document.getElementById("scroll_arrows").addEventListener('click', collapseLogo)
+document.getElementById("logo").addEventListener('click', expandLogo)
 
 var logoCollapsed = false;
 
 function collapseLogo() {
     document.getElementById("logo_disapear").style.opacity = "0";
     document.getElementById("logo_H").style.transform = "translate(" + -452.5 + "px, " + 0.5 + "px)"
-    //document.getElementById("logo").style.left = (0.3*document.getElementById("long").getBoundingClientRect().width/2 + 20) + "px"
-    //document.getElementById("logo").style.top = (0.3*document.getElementById("logo_M").getBoundingClientRect().height/2 + 20) + "px"
-    document.getElementById("logo").style.left = 14 + "%"
-    document.getElementById("logo").style.top = 5 + "%"
+    document.getElementById("logo").style.left = 170 + "px"
+    document.getElementById("logo").style.top = 50 + "px"
     document.getElementById("logo").style.transform = "scale(0.3) translate(-50%, -50%)"
+    document.getElementById("logo").style.maxWidth = 100000 + "%"
+    document.getElementById("logo").
     logoCollapsed = true;
+}
+
+function expandLogo() {
+    document.getElementById("logo_disapear").style.opacity = "1";
+    document.getElementById("logo_H").style.transform = "translate( 0,0)"
+    document.getElementById("logo").style.left = "50%"
+    document.getElementById("logo").style.top = "50%"
+    document.getElementById("logo").style.transform = "scale(1) translate(-50%, -50%)"
+    document.getElementById("logo").style.maxWidth = 80 + "%"
+    logoCollapsed = false;
 }
