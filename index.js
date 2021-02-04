d44c9226d15132e6c0d3507b55bf768e4ad30604
Marvin Fuchs <marfinn3@gmail.com>
2021-02-04 21:16:50 +0100

    Fix recize bug with logo

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ document.getElementById("scroll_arrows").addEventListener('click', collapseLogo)
 document.getElementById("logo").addEventListener('click', expandLogo)
 
 var logoCollapsed = false;
+var logoWidthExpanded = document.getElementById("logo").style.width;
 
 function collapseLogo() {
     document.getElementById("logo_disapear").style.opacity = "0";
     document.getElementById("logo_H").style.transform = "translate(" + -452.5 + "px, " + 0.5 + "px)"
     document.getElementById("logo").style.left = 170 + "px"
     document.getElementById("logo").style.top = 50 + "px"
-    document.getElementById("logo").style.transform = "scale(0.3) translate(-50%, -50%)"
-    document.getElementById("logo").style.maxWidth = 100000 + "%"
-    document.getElementById("logo").
+    document.getElementById("logo").style.width = "300px"
     logoCollapsed = true;
 }
 
@@ -19,7 +18,6 @@ function expandLogo() {
     document.getElementById("logo_H").style.transform = "translate( 0,0)"
     document.getElementById("logo").style.left = "50%"
     document.getElementById("logo").style.top = "50%"
-    document.getElementById("logo").style.transform = "scale(1) translate(-50%, -50%)"
-    document.getElementById("logo").style.maxWidth = 80 + "%"
+    document.getElementById("logo").style.width = logoWidthExpanded
     logoCollapsed = false;
 }
