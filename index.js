e71da9163902f768ab20d3d9e60283cc9c021dfd
Marvin Fuchs <marfinn3@gmail.com>
2022-07-18 16:37:51 +0200

    Update link hover

---
+++ index.js
@@ -0,0 +1,39 @@
+var background = document.getElementById("background")
+var backgroundRect = background.getBoundingClientRect()
+
+var background_x = 0
+var background_y = 0
+var background_x_middle = 0
+var background_y_middle = 0
+
+function linkHoverStart(link) {
+    let linkRect = link.getBoundingClientRect()
+    background.style.transform = "scale(1)"
+
+    background_x_middle = linkRect.x + linkRect.width/2 
+    background_y_middle = linkRect.y + linkRect.height/2
+    background_x = background_x_middle - (60 * (4/3))/2
+    background_y = background_y_middle - (60 * (4/3))/2
+
+    background.style.left = background_x + "px"
+    background.style.top = background_y + "px"
+}
+
+function linkHoverEnd(link) {
+    background.style.transform = "scale(0)"
+}
+
+onmousemove = (event) => {
+    backgroundRect = background.getBoundingClientRect()
+
+    x_distance = event.clientX - background_x_middle
+    y_distance = event.clientY - background_y_middle
+
+    distance = Math.sqrt(Math.pow(x_distance, 2) + Math.pow(y_distance, 2))
+
+    strength = Math.exp(-distance/80)
+
+    background.style.left = (background_x + x_distance * strength) + "px"
+    background.style.top = (background_y + y_distance * strength) + "px"
+
+}
