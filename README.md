25a784e9dfa3572173a6d781f4fb121f5e5a384f
Marvin Fuchs <marfinn3@gmail.com>
2022-12-09 14:50:14 +0100

    Add new projects and move old website.

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Welcome to my github. Check out my [website](marvin-fuchs.de) (german) for more about my projects and me! 🚀
 
 🎈 Currently working on:
+
 - [AdventOfCode](https://github.com/habetuz/AdventOfCode)
 - [Distribute](https://github.com/habetuz/Distribute)
--- old/index.js
+++ old/index.js
@@ -101,18 +101,18 @@ var videoSources = [
 ]
 
 var imageSources =  [
-    "assets/images/12.jpg",
-    "assets/images/11.jpg",
-    "assets/images/10.jpg",
-    "assets/images/9.jpg",
-    "assets/images/8.jpg",
-    "assets/images/7.jpg",
-    "assets/images/6.jpg",
-    "assets/images/5.jpg",
-    "assets/images/4.jpg",
-    "assets/images/3.jpg",
-    "assets/images/2.jpg",
-    "assets/images/1.jpg",
+    "../assets/images/12.jpg",
+    "../assets/images/11.jpg",
+    "../assets/images/10.jpg",
+    "../assets/images/9.jpg",
+    "../assets/images/8.jpg",
+    "../assets/images/7.jpg",
+    "../assets/images/6.jpg",
+    "../assets/images/5.jpg",
+    "../assets/images/4.jpg",
+    "../assets/images/3.jpg",
+    "../assets/images/2.jpg",
+    "../assets/images/1.jpg",
 
 
     ]
